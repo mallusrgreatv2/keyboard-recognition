c04e35ff6c25aabce0c15cb60c19fcf39e7c23b8
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2021-07-20 15:34:53 +0530

    Add files via upload

---
+++ assets/javascript/mobile.js
@@ -0,0 +1,20 @@
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
+
+window.addEventListener("load", () => {
+    let mobile = detectMob();
+    if(mobile) return window.location.replace("https://recognition.netlify.app/mobile.html");
+})
